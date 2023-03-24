526a553a8715b018f40bf4d612167056e43a83c1
Svyt <peogne@gmail.com>
2023-03-25 01:07:15 +0400

    Add function registercommands.

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,28 @@ import { Logger } from "tslog";
 import { readFileSync } from "fs";
 import { Client, Events, GatewayIntentBits, Interaction } from "discord.js";
 
-import { 
-    Command,
-    Ping 
+import {
+    Ping
 } from "@command";
 import { ConfigService } from "@service";
 import { ServiceManager } from "@manager";
-import { registerCommandsInGlobal, registerCommandsInGuild } from "@register";
+import { registerCommands } from "@register";
 
 
-/**
+/** ё
  * The function starts the entire project.
+ * Reading the config from the file.
  */
 async function main() {
 
+    const commands = [
+        new Ping()
+    ];
+
     const logger = new Logger();
-    /**
-     * Reading the config from the file.
-     */
+
     const buffer = readFileSync("config.json");
     const config: ConfigService.Config = JSON.parse(buffer.toString());
-
     logger.debug("The config is loaded.")
 
     const configService = new ConfigService(config);
@@ -31,7 +32,8 @@ async function main() {
         .addService(configService)
         .addService(logger);
 
-    logger.debug("Services were added.")
+    const mapOfCommands = await registerCommands(config.token, config.clientId, config.guildIds, commands);
+    logger.debug("The commands were registered.")
 
     const client = new Client({
         intents: [
@@ -41,44 +43,17 @@ async function main() {
             GatewayIntentBits.MessageContent,
             GatewayIntentBits.GuildMessages
         ]
-    });
-
-    const commands = [
-        new Ping()
-    ];
-    const guildCommands = [];
-    const globalCommands = [];
-
-    const mapOfCommands = new Map<string, Command>();
-
-    for (const command of commands) {
-        mapOfCommands.set(command.data.name, command);
-        const json = command.data.toJSON();
-        command.isGlobal ? globalCommands.push(json) : guildCommands.push(json);
-    }
-
-    await registerCommandsInGlobal(config.token, config.clientId, globalCommands);
-    
-    for (const guild of config.guildIds) {
-        await registerCommandsInGuild(config.token, config.clientId, guild, globalCommands);
-    }
-
-    logger.debug("The commands were registered.")
-
-    client.addListener(Events.InteractionCreate, async (interaction: Interaction) => {
+    }).addListener(Events.InteractionCreate, async (interaction: Interaction) => {
         if (!interaction.isChatInputCommand()) return;
 
         const command = mapOfCommands.get(interaction.commandName);
         if (command === undefined) return;
-        
-        await command.execute(interaction, serviceManager);
-    });
 
-    client.addListener(Events.ClientReady, () => logger.debug("Connected."))
+        await command.execute(interaction, serviceManager);
+    }).addListener(Events.ClientReady, () => logger.debug("Connected."))
 
     logger.debug("Connecting...");
     await client.login(config.token);
-
 }
 
 
--- src/register/index.ts
+++ src/register/index.ts
@@ -1,4 +1,5 @@
-import {REST, Routes} from "discord.js";
+import { Command } from "@command";
+import { REST, Routes } from "discord.js";
 
 
 export async function registerCommandsInGuild(
@@ -7,12 +8,12 @@ export async function registerCommandsInGuild(
     guildId: string,
     commands: any[]) {
     // Construct and prepare an instance of the REST module
-    const rest = new REST({version: '10'}).setToken(token);
+    const rest = new REST({ version: '10' }).setToken(token);
 
     // The put method is used to fully refresh all commands in the guild with the current set
     return await rest.put(
         Routes.applicationGuildCommands(clientId, guildId),
-        {body: commands},
+        { body: commands },
     );
 }
 
@@ -21,11 +22,38 @@ export async function registerCommandsInGlobal(
     clientId: string,
     commands: any[]) {
     // Construct and prepare an instance of the REST module
-    const rest = new REST({version: '10'}).setToken(token);
+    const rest = new REST({ version: '10' }).setToken(token);
 
     // The put method is used to fully refresh all commands in the guild with the current set
     return await rest.put(
         Routes.applicationCommands(clientId),
-        {body: commands},
+        { body: commands },
     );
+}
+
+
+export async function registerCommands(
+    token: string,
+    clientId: string,
+    guildIds: string[],
+    commands: Command[]
+): Promise<Map<string, Command>> {
+    const guildCommands = [];
+    const globalCommands = [];
+
+    const map = new Map<string, Command>();
+
+    for (const command of commands) {
+        map.set(command.data.name, command);
+        const json = command.data.toJSON();
+        command.isGlobal ? globalCommands.push(json) : guildCommands.push(json);
+    }
+
+    await registerCommandsInGlobal(token, clientId, globalCommands);
+
+    for (const guildId of guildIds) {
+        await registerCommandsInGuild(token, clientId, guildId, globalCommands);
+    }
+
+    return map;
 }
